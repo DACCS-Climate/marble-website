347b7fd2047c5e1fc67b80ef88639b0ed46aedd0
mishaschwartz <4380924+mishaschwartz@users.noreply.github.com>
2024-04-29 11:46:23 -0400

    allow committee to remain on page reload

--- templates/site/js/about.js
+++ templates/site/js/about.js
@@ -1,11 +1,13 @@
 let committee_options = [
     {
         "div": document.getElementById("executiveCommittee"),
-        "button": document.getElementById("executiveMenuButton")
+        "button": document.getElementById("executiveMenuButton"),
+        "hash": "executive"
     },
     {
         "div": document.getElementById("scientificCommittee"),
-        "button": document.getElementById("scientificMenuButton")
+        "button": document.getElementById("scientificMenuButton"),
+        "hash": "scientific"
     },
 ]
 
@@ -14,6 +16,7 @@ committee_options.forEach((option) => {
         // only continue if changing which committee is shown (replace will return false if d-none is not present)
         if (option.div.classList.replace("d-none", "d-flex")) {
             option.button.classList.add("text-primary"); // change button color
+            window.location.hash = option.hash;
             committee_options.forEach((other) => {
                 if (other !== option) {
                     other.div.classList.replace("d-flex", "d-none"); // hide all other committees
@@ -23,3 +26,15 @@ committee_options.forEach((option) => {
         }
     }
 })
+
+// show selected committee if hash is present when page is loaded
+window.onload = () => {
+    if (window.location.hash) {
+        console.log(window.location.hash)
+        committee_options.forEach((option) => {
+            if (`#${option.hash}` === window.location.hash) {
+                option.button.click();
+            }
+        })
+    }
+}
