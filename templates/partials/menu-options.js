0ce5491babee1bb1cced2b5b0ed631731e274902
alexandercyu <alexanderc.yu@utoronto.ca>
2024-06-13 14:57:12 -0400

    Update menu-options.js
    Added condition that will make this file usable on Nodes page as well as Home page

--- templates/partials/menu-options.js
+++ templates/partials/menu-options.js
@@ -1,5 +1,3 @@
-console.log(options);
-//console.log(options["0"].hash);
 options.forEach((option) => {
     if(option.div){
         option.button.onclick = () => {
@@ -17,22 +15,17 @@ options.forEach((option) => {
         }
     }
     else{
-        console.log(option);
         option.button.onclick = () => {
             // only continue if changing which option is shown (replace will return false if d-none is not present)
-
-                option.button.classList.add("text-primary"); // change button color
-                window.location.hash = option.hash;
-                options.forEach((other) => {
-                    if (other !== option) {
-                        other.button.classList.remove("text-primary"); // revert button color of other options
-                    }
-                })
-          
-                
+            option.button.classList.add("text-primary"); // change button color
+            window.location.hash = option.hash;
+            options.forEach((other) => {
+                if (other !== option) {
+                    other.button.classList.remove("text-primary"); // revert button color of other options
+                }
+            })
         }
     }
-   
 })
 
 // show selected option if hash is present when page is loaded
