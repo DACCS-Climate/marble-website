04a5d1e53228027d66db966e18b148dfbe775c4f
alexandercyu <alexanderc.yu@utoronto.ca>
2024-06-13 11:29:28 -0400

    Update menu-options.js
    Add condition based on presence of "div" entry so the function will behave differently on the Home page and Node page

--- templates/partials/menu-options.js
+++ templates/partials/menu-options.js
@@ -1,17 +1,38 @@
+console.log(options);
+//console.log(options["0"].hash);
 options.forEach((option) => {
-    option.button.onclick = () => {
-        // only continue if changing which option is shown (replace will return false if d-none is not present)
-        if (option.div.classList.replace("d-none", "d-flex")) {
-            option.button.classList.add("text-primary"); // change button color
-            window.location.hash = option.hash;
-            options.forEach((other) => {
-                if (other !== option) {
-                    other.div.classList.replace("d-flex", "d-none"); // hide all other options
-                    other.button.classList.remove("text-primary"); // revert button color of other options
-                }
-            })
+    if(option.div){
+        option.button.onclick = () => {
+            // only continue if changing which option is shown (replace will return false if d-none is not present)
+            if (option.div.classList.replace("d-none", "d-flex")) {
+                option.button.classList.add("text-primary"); // change button color
+                window.location.hash = option.hash;
+                options.forEach((other) => {
+                    if (other !== option) {
+                        other.div.classList.replace("d-flex", "d-none"); // hide all other options
+                        other.button.classList.remove("text-primary"); // revert button color of other options
+                    }
+                })
+            }
         }
     }
+    else{
+        console.log(option);
+        option.button.onclick = () => {
+            // only continue if changing which option is shown (replace will return false if d-none is not present)
+
+                option.button.classList.add("text-primary"); // change button color
+                window.location.hash = option.hash;
+                options.forEach((other) => {
+                    if (other !== option) {
+                        other.button.classList.remove("text-primary"); // revert button color of other options
+                    }
+                })
+          
+                
+        }
+    }
+   
 })
 
 // show selected option if hash is present when page is loaded
