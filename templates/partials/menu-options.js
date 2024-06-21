261d51290f11301311971de2e4f4db5a3200cee8
alexandercyu <alexanderc.yu@utoronto.ca>
2024-06-21 13:11:26 -0400

    Update menu-options.js
    - Changed function so duplicated code removed
    - Added condition in area where "other" menu items get the primary colour class removed
        - Added condition so it won't cause an error when the array element "div" is not found

--- templates/partials/menu-options.js
+++ templates/partials/menu-options.js
@@ -1,26 +1,14 @@
 options.forEach((option) => {
-    if(option.div){
-        option.button.onclick = () => {
-            // only continue if changing which option is shown (replace will return false if d-none is not present)
-            if (option.div.classList.replace("d-none", "d-flex")) {
-                option.button.classList.add("text-primary"); // change button color
-                window.location.hash = option.hash;
-                options.forEach((other) => {
-                    if (other !== option) {
-                        other.div.classList.replace("d-flex", "d-none"); // hide all other options
-                        other.button.classList.remove("text-primary"); // revert button color of other options
-                    }
-                })
-            }
-        }
-    }
-    else{
-        option.button.onclick = () => {
-            // only continue if changing which option is shown (replace will return false if d-none is not present)
+    option.button.onclick = () => {
+        // only continue if changing which option is shown (replace will return false if d-none is not present)
+        if (!option.div || option.div.classList.replace("d-none", "d-flex")) {
             option.button.classList.add("text-primary"); // change button color
             window.location.hash = option.hash;
             options.forEach((other) => {
                 if (other !== option) {
+                    if(other.div){
+                        other.div.classList.replace("d-flex", "d-none"); // hide all other options
+                    }
                     other.button.classList.remove("text-primary"); // revert button color of other options
                 }
             })
@@ -28,13 +16,23 @@ options.forEach((option) => {
     }
 })
 
-// show selected option if hash is present when page is loaded
-window.onload = () => {
-    if (window.location.hash) {
-        options.forEach((option) => {
-            if (`#${option.hash}` === window.location.hash) {
-                option.button.click();
-            }
-        })
+setInitialOption = () => {
+    window.onload = () => {
+        if (window.location.hash) {
+            options.forEach((option) => {
+                if (`#${option.hash}` === window.location.hash) {
+                    option.button.click();
+                }
+            })
+        }
     }
+}
+
+// show selected option if hash is present when page is loaded
+window.onload = setInitialOption
+
+// call this function manually in case the window has already loaded since then the onload event will have already
+// fired. This can happen if this script is loaded asynchronously.
+if (document.readyState === "complete") {
+    setInitialOption()
 }
